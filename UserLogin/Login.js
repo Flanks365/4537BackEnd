0d095ab335ef4f48974e03299825984c5006a222
Flanks365 <ble16@my.bcit.ca>
2025-03-14 11:49:01 -0700

    Update Login.js

--- UserLogin/Login.js
+++ UserLogin/Login.js
@@ -31,7 +31,7 @@ class Database {
         if (!this.connection) {
             this.connection = mysql.createConnection({
                 host: process.env.HOST,
-                user: process.env.USER,
+                user: process.env.USERNAME,
                 password: process.env.PASSWORD,
                 database: process.env.DATABASE,
                 port: process.env.PORT
