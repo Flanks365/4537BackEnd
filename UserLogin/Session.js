6d196019a7169a934390fa6f8294a8d08a849551
Flanks365 <ble16@my.bcit.ca>
2025-03-26 11:21:59 -0700

    Update Session.js

--- UserLogin/Session.js
+++ UserLogin/Session.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const http = require('http');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
