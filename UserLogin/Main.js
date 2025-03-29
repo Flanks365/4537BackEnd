9da2b5b19749a5dce0a06cad6c828d40e551f9c8
Mike Doswell <mike.doswell@gmail.com>
2025-03-29 14:53:23 -0700

    testing apistats

--- UserLogin/Main.js
+++ UserLogin/Main.js
@@ -277,7 +277,7 @@ app.post('/api/v1/gradeAnswer', async (req, res) => {
 
 app.get('/api/v1/apiStats', async (req, res) => {
   console.log("GET /apiStats");
-  res.json(req)
+  console.log(req)
   try {
     const result = await apiStatsUtils.getUsage(req, res)
     res.json(result)
