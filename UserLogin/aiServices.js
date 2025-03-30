0381a3bcfb2fdd56bab3123aea602369f0587024
Mike Doswell <mike.doswell@gmail.com>
2025-03-30 10:11:40 -0700

    fixing transcribeQuestion

--- UserLogin/aiServices.js
+++ UserLogin/aiServices.js
@@ -14,6 +14,7 @@ const API_KEY = process.env.AI_API_KEY;
 class aiUtils{
     static async transcribeQuestion(req, res) {
         try {
+            throw Error('req: ' + JSON.stringify(req))
             const userId = jwt.verify(req.token, secretKey, { algorithms: ['HS256'] }).userId;
             await apiStatsUtils.incrementUsage(userId, '/api/v1/transcribeQuestion', 'POST')
 
